00f4ff876b304a8775c67502f69be0d0497b8d85
Francis Kiarie <fkiarie100@gmail.com>
2026-06-25 18:28:41 +0300

    Add enum classes for DivisionType, MemberRank, SpecialistTrack, MemberStatus, and DutyType

---
+++ app/models/enums.py
@@ -0,0 +1,38 @@
+import enum
+
+class DivisionType(str, enum.Enum):
+    adult = "adult"
+    cadet = "cadet"
+    nursing = "nursing"
+    dolphin_pod = "dolphin_pod"
+
+class MemberRank(str, enum.Enum):
+    member = "member"
+    corporal = "corporal"
+    sergeant = "sergeant"
+    acting_div_officer = "acting_div_officer"
+    div_officer = "div_officer"
+    div_superintendent = "div_superintendent"
+
+class SpecialistTrack(str, enum.Enum):
+    none = "none"
+    med_student = "med_student"
+    registered_surgeon = "registered_surgeon"
+    nursing_student = "nursing_student"
+    registered_nursing_officer = "registered_nursing_officer"
+    emt_student = "emt_student"
+    registered_paramedic = "registered_paramedic"
+
+class MemberStatus(str, enum.Enum):
+    active = "active"
+    suspended = "suspended"
+    discharged = "discharged"
+    transferred = "transferred"
+    deceased = "deceased"
+
+class DutyType(str, enum.Enum):
+    public_duty = "public_duty"
+    community_service = "community_service"
+    divisional_meeting = "divisional_meeting"
+    parade = "parade"
+    training = "training"
